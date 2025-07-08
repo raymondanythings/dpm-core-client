2881029085ad936eefa39e6ef38cb6ed9ee779fd
leejeongho <92032081+LeeJeongHooo@users.noreply.github.com>
2025-07-08 17:39:46 +0900

    feat: 디자인 토큰 및 공용 컴포넌트 (#1)
    * chore: color token & typography
    
    * feat: button 컴포넌트
    
    * chore: 기본 color 설정
    
    * feat: drawer 컴포넌트
    
    * chore: green color 추가
    
    * feat: full button 추가
    
    * fix: Drawer 수정
    
    * feat: avatar 컴포넌트

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        {
+          text: [
+            "headline1",
+            "headline2",
+            "title1",
+            "title2",
+            "body1",
+            "body2",
+            "caption1",
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
